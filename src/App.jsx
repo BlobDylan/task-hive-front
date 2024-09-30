e5a1bf3ce8587e3a58aebf0047309e9f51549a91
Dylan Lewis <dylan.lewis@post.runi.ac.il>
2024-09-30 17:15:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+import DashboardPage from "./pages/DashboardPage";
+
+const MainLayout = () => {
+  const location = useLocation();
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+    >
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <MainLayout />
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+function DashboardPage() {
+  return (
+    <Box>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard</p>
+    </Box>
+  );
+}
+
+export default DashboardPage;
