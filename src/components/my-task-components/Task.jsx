4b671b43af95272be2fb9d69272552ea3f587ce4
Dylan Lewis <dylan.lewis@post.runi.ac.il>
2024-10-08 14:13:39 +0300

    edit dialogs

--- src/components/my-task-components/Task.jsx
+++ src/components/my-task-components/Task.jsx
@@ -8,6 +8,13 @@ import {
   AccordionDetails,
   AccordionSummary,
   IconButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Button,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
@@ -15,70 +22,186 @@ import EditIcon from "@mui/icons-material/Edit";
 
 import { useState } from "react";
 
-function LinearProgressWithLabel(props) {
+function MainTask(props) {
+  const [edittaskopen, setEdittaskopen] = useState(false);
+
+  const handleOpenEditTask = () => {
+    setEdittaskopen(true);
+  };
+
+  const handleCloseEditTask = () => {
+    setEdittaskopen(false);
+  };
+
   return (
-    <Box
-      sx={{
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        borderRadius: "5px",
-        ml: 1,
-        mr: 1,
-      }}
-    >
-      <IconButton>
-        <EditIcon />
-      </IconButton>
-      <Checkbox />
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2">Task</Typography>
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          borderRadius: "5px",
+          ml: 1,
+          mr: 1,
+        }}
+      >
+        <IconButton onClick={setEdittaskopen}>
+          <EditIcon />
+        </IconButton>
+        <Checkbox />
+        <Box sx={{ minWidth: 35 }}>
+          <Typography variant="body2">Task</Typography>
+        </Box>
+        <Box sx={{ width: "100%", mr: 1, ml: 1 }}>
+          <LinearProgress
+            variant="determinate"
+            {...props}
+            sx={{
+              "& .MuiLinearProgress-bar": {
+                backgroundColor: "text.primary",
+              },
+            }}
+          />
+        </Box>
+        <Box sx={{ minWidth: 35 }}>
+          <Typography variant="body2">{`${Math.round(
+            props.value
+          )}%`}</Typography>
+        </Box>
       </Box>
-      <Box sx={{ width: "100%", mr: 1, ml: 1 }}>
-        <LinearProgress
-          variant="determinate"
-          {...props}
-          sx={{
-            "& .MuiLinearProgress-bar": {
-              backgroundColor: "text.primary", // Change this to your desired color
-            },
-          }}
-        />
-      </Box>
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2">{`${Math.round(props.value)}%`}</Typography>
-      </Box>
-    </Box>
+
+      <Dialog
+        open={edittaskopen}
+        onClose={handleCloseEditTask}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            console.log(formJson);
+            handleCloseEditTask();
+          },
+        }}
+      >
+        <DialogTitle>Edit</DialogTitle>
+        <DialogContent>
+          <DialogContentText></DialogContentText>
+          <TextField
+            autoFocus
+            id="name"
+            margin="dense"
+            name="name"
+            label="name"
+            type="name"
+            defaultValue={"Sub Task"}
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            id="due-date"
+            margin="dense"
+            name="due-date"
+            type="date"
+            defaultValue={"2022-01-01"}
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseEditTask}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
 function SubTask() {
+  const [editsubtaskopen, setEditsubtaskopen] = useState(false);
+
+  const handleOpenEditSubTask = () => {
+    setEditsubtaskopen(true);
+  };
+
+  const handleCloseEditSubTask = () => {
+    setEditsubtaskopen(false);
+  };
+
   return (
-    <Box
-      sx={{
-        width: "80%",
-        display: "flex",
-        alignItems: "center",
-        borderRadius: "5px",
-        boxShadow: "0 0 5px 0 rgba(0,0,0,0.2)",
-        ml: 5,
-        mr: 5,
-        mt: 2,
-      }}
-    >
-      <IconButton>
-        <EditIcon />
-      </IconButton>
-      <Checkbox />
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2">Sub Task</Typography>
+    <>
+      <Box
+        sx={{
+          width: "80%",
+          display: "flex",
+          alignItems: "center",
+          borderRadius: "5px",
+          boxShadow: "0 0 5px 0 rgba(0,0,0,0.2)",
+          ml: 5,
+          mr: 5,
+          mt: 2,
+        }}
+      >
+        <IconButton onClick={handleOpenEditSubTask}>
+          <EditIcon />
+        </IconButton>
+        <Checkbox />
+        <Box sx={{ minWidth: 35 }}>
+          <Typography variant="body2">Sub Task</Typography>
+        </Box>
       </Box>
-    </Box>
+      <Dialog
+        open={editsubtaskopen}
+        onClose={handleCloseEditSubTask}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            console.log(formJson);
+            handleCloseEditSubTask();
+          },
+        }}
+      >
+        <DialogTitle>Edit</DialogTitle>
+        <DialogContent>
+          <DialogContentText></DialogContentText>
+          <TextField
+            autoFocus
+            id="name"
+            margin="dense"
+            name="name"
+            label="name"
+            type="name"
+            defaultValue={"Sub Task"}
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            id="due-date"
+            margin="dense"
+            name="due-date"
+            type="date"
+            defaultValue={"2022-01-01"}
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseEditSubTask}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
 function Task() {
   const [progress, setProgress] = useState(10);
+
   return (
     <Box sx={{ width: "100%" }}>
       <Accordion sx={{ width: "100%" }}>
@@ -87,7 +210,7 @@ function Task() {
           aria-controls="panel3-content"
           id="panel3-header"
         >
-          <LinearProgressWithLabel value={progress} />
+          <MainTask value={progress} />
         </AccordionSummary>
         <AccordionDetails sx={{ padding: 0 }}>
           <SubTask />
