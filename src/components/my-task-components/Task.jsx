12e46d17efd3670eec32a99cee0944f9f0028628
Dylan Lewis <dylan.lewis@post.runi.ac.il>
2024-10-09 11:14:38 +0300

    add dialogs

--- src/components/my-task-components/Task.jsx
+++ src/components/my-task-components/Task.jsx
@@ -45,10 +45,17 @@ function MainTask(props) {
           mr: 1,
         }}
       >
-        <IconButton onClick={setEdittaskopen}>
+        <IconButton onClick={handleOpenEditTask}>
           <EditIcon />
         </IconButton>
-        <Checkbox />
+        <Checkbox
+          sx={{
+            color: "defaultColor",
+            "&.Mui-checked": {
+              color: "text.primary",
+            },
+          }}
+        />
         <Box sx={{ minWidth: 35 }}>
           <Typography variant="body2">Task</Typography>
         </Box>
@@ -103,6 +110,7 @@ function MainTask(props) {
             id="due-date"
             margin="dense"
             name="due-date"
+            label="due-date"
             type="date"
             defaultValue={"2022-01-01"}
             fullWidth
@@ -146,7 +154,14 @@ function SubTask() {
         <IconButton onClick={handleOpenEditSubTask}>
           <EditIcon />
         </IconButton>
-        <Checkbox />
+        <Checkbox
+          sx={{
+            color: "defaultColor",
+            "&.Mui-checked": {
+              color: "text.primary",
+            },
+          }}
+        />
         <Box sx={{ minWidth: 35 }}>
           <Typography variant="body2">Sub Task</Typography>
         </Box>
@@ -184,6 +199,7 @@ function SubTask() {
             id="due-date"
             margin="dense"
             name="due-date"
+            label="due-date"
             type="date"
             defaultValue={"2022-01-01"}
             fullWidth
@@ -202,27 +218,85 @@ function SubTask() {
 function Task() {
   const [progress, setProgress] = useState(10);
 
+  const [openaddsubtask, setOpenaddsubtask] = useState(false);
+
+  const handleOpenaddsubtask = () => {
+    setOpenaddsubtask(true);
+  };
+
+  const handleCloseaddsubtask = () => {
+    setOpenaddsubtask(false);
+  };
+
   return (
-    <Box sx={{ width: "100%" }}>
-      <Accordion sx={{ width: "100%" }}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3-content"
-          id="panel3-header"
-        >
-          <MainTask value={progress} />
-        </AccordionSummary>
-        <AccordionDetails sx={{ padding: 0 }}>
-          <SubTask />
-          <SubTask />
-        </AccordionDetails>
-        <AccordionActions>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </AccordionActions>
-      </Accordion>
-    </Box>
+    <>
+      <Box sx={{ width: "100%" }}>
+        <Accordion sx={{ width: "100%" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+          >
+            <MainTask value={progress} />
+          </AccordionSummary>
+          <AccordionDetails sx={{ padding: 0 }}>
+            <SubTask />
+            <SubTask />
+          </AccordionDetails>
+          <AccordionActions>
+            <IconButton onClick={handleOpenaddsubtask}>
+              <AddIcon />
+            </IconButton>
+          </AccordionActions>
+        </Accordion>
+      </Box>
+
+      <Dialog
+        open={openaddsubtask}
+        onClose={handleCloseaddsubtask}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            console.log(formJson);
+            handleCloseaddsubtask();
+          },
+        }}
+      >
+        <DialogTitle>New Sub-Task</DialogTitle>
+        <DialogContent>
+          <DialogContentText></DialogContentText>
+          <TextField
+            autoFocus
+            id="name"
+            margin="dense"
+            name="name"
+            label="name"
+            type="name"
+            defaultValue={"Sub Task"}
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            id="due-date"
+            margin="dense"
+            name="due-date"
+            label="due-date"
+            type="date"
+            defaultValue={"2022-01-01"}
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseaddsubtask}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
--- src/components/my-task-components/TaskFolder.jsx
+++ src/components/my-task-components/TaskFolder.jsx
@@ -1,33 +1,99 @@
+import { useState } from "react";
 import {
   Accordion,
   AccordionActions,
   AccordionDetails,
   AccordionSummary,
   IconButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Button,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Task from "./Task";
 
 function TaskFolder() {
+  const [openaddtask, setOpenaddtask] = useState(false);
+
+  const handleOpenaddtask = () => {
+    setOpenaddtask(true);
+  };
+
+  const handleCloseaddtask = () => {
+    setOpenaddtask(false);
+  };
+
   return (
-    <Accordion sx={{ width: "100%", backgroundColor: "primary.main" }}>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel3-content"
-        id="panel3-header"
+    <>
+      <Accordion sx={{ width: "100%", backgroundColor: "primary.main" }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Folder
+        </AccordionSummary>
+        <AccordionDetails>
+          <Task />
+        </AccordionDetails>
+        <AccordionActions>
+          <IconButton onClick={handleOpenaddtask}>
+            <AddIcon />
+          </IconButton>
+        </AccordionActions>
+      </Accordion>
+
+      <Dialog
+        open={openaddtask}
+        onClose={handleCloseaddtask}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            console.log(formJson);
+            handleCloseaddtask();
+          },
+        }}
       >
-        Folder
-      </AccordionSummary>
-      <AccordionDetails>
-        <Task />
-      </AccordionDetails>
-      <AccordionActions>
-        <IconButton>
-          <AddIcon />
-        </IconButton>
-      </AccordionActions>
-    </Accordion>
+        <DialogTitle>New Task</DialogTitle>
+        <DialogContent>
+          <DialogContentText></DialogContentText>
+          <TextField
+            autoFocus
+            id="name"
+            margin="dense"
+            name="name"
+            label="name"
+            type="name"
+            defaultValue={"Sub Task"}
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            id="due-date"
+            margin="dense"
+            name="due-date"
+            label="due-date"
+            type="date"
+            defaultValue={"2022-01-01"}
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseaddtask}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
